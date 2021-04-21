58b10db4940361ae248db7c06b631acb58d22858
alaajaradat0 <alaajaradat758@gmail.com>
2021-04-21 23:58:31 +0300

    delet methods

---
+++ main.js
@@ -0,0 +1,103 @@
+console.log(arrowTitle)
+/* START CODE UNDER THIS LINE */
+
+
+console.log('welcome alaa')                        
+
+
+console.log(9+1*5)
+console.log(9%2)
+console.log(9/3)*(10-2)
+console.log(21/3+3*9*9+5)
+console.log(36/9+48-10/2)
+
+//**************** *//
+
+const name ='john';
+const age = '10';
+const cons  = "constant"
+const variable1 = true 
+const true2 = true 
+
+//************** *//
+
+let color = 'white';
+const positiveNumber = 10;
+const food = 'fries potato' 
+const phoneNumber = "0777201727";
+
+//***************** *//
+console.log(10+'10');
+console.log("10"+"10");
+console.log(10+10+"5");
+console.log("Hello"+1);
+console.log("10"+5+5);
+console.log("20"-10);
+console.log("9"-"5");
+console.log("19" - "13" + 17);
+console.log(5+6+"4"+9-4-2);
+//*************** *//
+
+console.log("ALaa"+" jaradat"+" male"+" Jordanian")
+
+//************************** *//
+
+console.log(60*60*24*30);
+
+//************************ */
+
+let a=29;
+let b=3 ;
+let c =10;
+
+console.log(c-b*c+a);
+
+//***********************0 *//
+
+const tip = 0.1 ;
+const taxRate = 0.08;
+const CCP = 6;
+
+const Pwtax = CCP*taxRate;
+const PwTip = CCP*tip;
+
+
+
+console.log((CCP+PwTip+Pwtax)*2);
+
+//*************************** *//
+
+console.log((24+16+29)/3);
+
+//************************ *//
+
+const L = 10;
+const W = 5;
+
+console.log("area="+(L*W))
+console.log("Perimetere="+(2*L)+(2*W));
+
+//******************** *//
+const Bay = 3.14;
+const Radius = 5;
+
+console.log("circumference = "+ ((2*Radius)*Bay))
+console.log("diameter="+(2*Radius))
+
+//**************** *//
+
+
+console.log((30*1.8)+32);
+
+//finish **********//
+
+const q=7;
+const x=9;
+console.log(q+x);
+
+
+
+
+
+
+
